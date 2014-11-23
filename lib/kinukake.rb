69bdaf1176500c5d2d1b71847e7ecc7a35ffea48
Kazutaka Sankai <Sankai_Kazutaka@ogis-ri.co.jp>
2014-11-23 23:01:10 +0900

    for version 0.0.3

--- lib/kinukake.rb
+++ lib/kinukake.rb
@@ -3,20 +3,32 @@ require "kinukake/version"
 require "open3"
 
 module Kinukake
-  def run (command, *params)
+
+def run (command, *params)
     
+    # コマンドはアプリケーションディレクトリのSHELL配下の前提
+    command = 'SHELL/' + command
+    
+    # コマンドの編集
     params.each do | param |
-      command = 'SHELL/' + command + ' ' + param.to_s    
+      command = command + ' ' + param.to_s    
     end 
     
     res = nil
     
+    # コマンドの実行
     Open3.popen3(command) {|i, o, e, t|
       i.close
+      
+      # エラー出力に何か出力されていたら例外を生成する
+      error_message = e.read
+      if error_message.length > 0 
+        raise error_message
+      end
       res = o.read
       }
     
     return res
   end
-
+  
 end
--- lib/kinukake/version.rb
+++ lib/kinukake/version.rb
@@ -1,3 +1,3 @@
 module Kinukake
-  VERSION = "0.0.1"
+  VERSION = "0.0.3"
 end
